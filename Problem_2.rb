872228b3a2e83279e9ef64307eb897b687d1f82a
Mueez Afzal <mueez.afzal@devden.io>
2021-07-30 18:08:25 +0500

    Restyling

--- Problem_2.rb
+++ Problem_2.rb
@@ -2,24 +2,17 @@
     s = s.split(",") #Split to make an array of Dominos
     num = 0
     max = 0
-
     for i in 0..(s.length-2) do #loop to traverse till 2nd last
-
       if s[i][2]==s[i+1][0]     #Checking R and L of Dominos
         num==0 ? num+=2 : num+=1#If it is 1st combination the add 2 else 1
-
       elsif num>max
         max=num      #Changing Max length Combinatin
         num=0
-
       end
-
     end
-
     if max<num      #for the last combination to chack max
       max=num
     end
-
     p max
   end
 
