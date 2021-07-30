88c556813efdb5ddfcbdd394809bf4db0b1fe942
Mueez Afzal <mueez.afzal@devden.io>
2021-07-30 18:05:54 +0500

    Restyling

--- Problem_1.rb
+++ Problem_1.rb
@@ -1,8 +1,9 @@
 def survival_index(n,k)
+
   if n.is_a?(Integer) && k.is_a?(Integer) &&
-     n>0 && k>=0     #Check to prevent exceptional values
+    n > 0 && k >= 0     #Check to prevent exceptional values
     arr = Array(1..n)
-    i=k-1            #traversal variable to skip K-1th places
+    i = k-1            #traversal variable to skip K-1th places
     until arr.length == 1 do #Loop until the last person remains
       arr.delete_at(i)
       i=(i+k-1)%n    #Incrementing 'i' so it remains in loop
@@ -10,7 +11,7 @@ def survival_index(n,k)
   else
     p "N OR K IS NOT VALID"
   end
-    arr
+  arr
 end
 
 p survival_index(3,2)
--- Problem_3.rb
+++ Problem_3.rb
@@ -1,23 +1,24 @@
 def isValid(s)
   if 1<= s.length && s.length <= 10**5 &&
-     s==s[/[a-zA-Z]+/] #Checking Exceptions
+    s==s[/[a-zA-Z]+/] #Checking Exceptions
     string=s
     s= s.split("").uniq #unique List of letter
     sherlock = Hash.new
-    s.each {|i| sherlock[i] = string.gsub(i).count} #Hash to save length of each letter
-    arr= Array.new(sherlock.values)
-    if arr.length == 1
+    s.each { |i| sherlock[i] = string.gsub(i).count } #Hash to save length of each letter
+    arr= Array.new(sherlock.values) #Array of length of All characters
+    if arr.length == 1  #if All have same length then 'Yes'
       return "Yes"
-    else
+    else                #Else check after removal of one letter
       index=arr.each_with_index.max[1]
-      arr[index]=arr.max-1
-      if arr.uniq.length == 1
+      arr[index]=arr.max-1 #Remove one letter of max length letter
+      if arr.uniq.length == 1 #Checking Same length or not
         return "Yes"
       else
         return "NO"
       end
     end
   end
-
 end
+
+
 p isValid("aabbbccc")
